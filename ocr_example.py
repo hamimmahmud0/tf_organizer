83a820bc974c577a01dcc765bf4feee3727074ee
hamimmahmud0 <hamimmahmud0@gmail.com>
2025-04-02 12:38:01 +0600

    before gpu support attempt

--- ocr_example.py
+++ ocr_example.py
@@ -1,17 +1,22 @@
 import keras_ocr
+import matplotlib.pyplot as plt
 
 def extract_text_from_image(image_path):
     # Create a Keras-OCR pipeline
     pipeline = keras_ocr.pipeline.Pipeline()
     
     # Read the image
-    image = keras_ocr.tools.read(image_path)
+    images = [keras_ocr.tools.read(image_path)]
     
     # Recognize text in the image
-    predictions = pipeline.recognize([image])
+    prediction_groups = pipeline.recognize(images)
     
-    for text, bounding_box in predictions[0]:
-        print(text)
+    
+    fig, axs = plt.subplots(nrows=len(images), figsize=(20, 20))
+
+    for ax, image, predictions in zip([axs], images, prediction_groups):
+        keras_ocr.tools.drawAnnotations(image=image, predictions=predictions, ax=ax)
+    plt.savefig('output.png')
 
 
 if __name__ == "__main__":
